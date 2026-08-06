9b73e942be8067139c4c7a2dd1d343a8f98f6508
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-08-06 01:24:12 -0300

    Finalizei a edição de usuário

--- src/controllers/editUserController.ts
+++ src/controllers/editUserController.ts
@@ -6,15 +6,21 @@ export const editUser = (req: Request, res: Response) => {
     const { id, username, email, password } = req.body as UserEdit;
 
     if(!id) {
-        return res.status(400).json({error: 'User ID is mandatory. '})
+        return res.status(400).json({error: 'User ID is mandatory.'})
     }
 
     const userIndex = usersDb.findIndex((user) => user.id === id);
 
     if(userIndex === - 1) {
-         res.status(404).json({ error: "User not found." });
+        return res.status(404).json({ error: "User not found." });
     };
 
-    usersDb[userIndex] 
+    usersDb[userIndex] = {
+        ...usersDb[userIndex],
+        username,
+        email,
+        password
+    }
 
+    return res.status(200).json({message: 'User successfully edited.'})
 }
