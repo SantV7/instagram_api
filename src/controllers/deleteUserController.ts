ab3862936ca56c267ddc2a62047a990c5cf874da
Vinícius dos Santos Oliveira <3izuna@gmail.com>
2026-08-13 18:01:55 -0300

    Crud simples em uma API com node.js e Express

--- src/controllers/deleteUserController.ts
+++ src/controllers/deleteUserController.ts
@@ -1,12 +1,9 @@
 import { type Request, type Response } from 'express';
-import type { DeleteUser } from '../types/users.type.ts';
 import { usersDb } from '../../app.ts';
 
 
 export const deleteUser = (req: Request, res: Response) => {
-    const { id } = req.body as DeleteUser;
-
-    const userIndex = usersDb.findIndex((user) => user.id === id);
+    const userIndex = res.locals.userIndex
 
     usersDb.splice(userIndex, 1)
 
